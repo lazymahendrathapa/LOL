709d379332fca968c66b65319f424d05b432baf9
Mahendra Thapa <Mahendra@fusemachines.com>
2017-02-01 14:09:45 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lazy Optimistic Learner (LOL)
---
+++ src/main/java/machinelearning/neuralnetwork/Application.java
@@ -0,0 +1,9 @@
+package machinelearning.neuralnetwork;
+
+public class Application {
+
+	public static void main(String[] args){
+		
+		System.out.println("All setup");
+	}
+}
